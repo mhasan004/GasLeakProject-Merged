4245b36874c2a06353c844f8bbe98af8ca50aa89
Mahmudul Hasan <mhasan0047@gmail.com>
2020-02-25 00:51:46 -0500

    filtered the census demographic data

---
+++ modify_changingDemographicsCSV.py
@@ -0,0 +1,64 @@
+import plotly.express as px
+import numpy as np
+import pandas as pd
+import textwrap  
+
+demoCsvFile = "DataFiles/Crime and Demographics/ACS_10_5YR_S0101_with_ann.csv"
+newCSV      = "DataFiles/Crime and Demographics/filtered_CensusTract_Demographic_Data.csv"
+demoDF  = pd.read_csv(demoCsvFile)  
+targetCounties = ["Bronx County", "Westchester County", 'New York County', 'Queens County', 'Kings County', 'Richmond County' ]
+
+print(demoDF)
+dfCols = list(demoDF.columns)
+
+
+# DROPPING ALL COLUMNS THAT LISTS THE MARGIN OF ERROR
+deleteCols = []
+for col in range(0, len(list(demoDF.columns))):
+    secondColName = demoDF.iloc[0][dfCols[col]]
+    secondStr = secondColName.split("; ")
+    if len(secondStr) >1:
+        if secondStr[1] == "Margin of Error":
+            deleteCols.append(dfCols[col])
+demoDF = demoDF.drop(columns=deleteCols)
+print(demoDF)
+dfCols = list(demoDF.columns)
+
+# DROPPING ALL COLUMNS THAT LISTS len(demoDF)-2 number of (X)'s...aka UNKNOWNS
+deleteCols = []
+for col in range(0, len(list(demoDF.columns))):
+    count = 0    
+    print("--- Filtering Col: "+str(col)+" / "+str(len(dfCols)-1))    
+    for row in range(0, len(demoDF)):
+        if demoDF.iloc[row][dfCols[col]] == "(X)":
+            count = count +1
+        if count == len(demoDF)-2:              # since proces tkes so long, it made it -2 the len of the df. if there are that many x's, col is useless so delete it
+            deleteCols.append(dfCols[col])
+        if row == 2 and count == 0:
+            continue
+
+demoDF = demoDF.drop(columns=deleteCols)
+print(demoDF)
+dfCols = list(demoDF.columns)
+
+# DROPPING ALL ROWS THAT ARNT IN NYC
+deleteRows = []
+for row in range(0, len(demoDF)):
+    colStr = demoDF.iloc[row]["GEO.display-label"]
+    secondStr = colStr.split(", ")
+    if len(secondStr)>1:
+        countyStr = secondStr[1]
+        if countyStr not in targetCounties:
+            deleteRows.append(demoDF.index[demoDF['GEO.display-label'] == colStr].tolist()[0])
+
+demoDF = demoDF.drop(index=deleteRows)
+demoDF = demoDF.reset_index(drop=True)
+
+demoDF.to_csv(newCSV, index=False )
+
+
+
+
+
+    
+
--- parallel.py
+++ parallel.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 import textwrap  
 
-df = px.data.iris()
-print(df)
-fig = px.parallel_coordinates(
-    df, 
-    color="species_id",
-    dimensions=['sepal_width', 'sepal_length', 'petal_width','petal_length'],
-    color_continuous_scale=px.colors.diverging.Tealrose,
-    color_continuous_midpoint=2
-)
-fig.show()
+# df = px.data.iris()
+# print(df)
+# fig = px.parallel_coordinates(
+#     df, 
+#     color="species_id",
+#     dimensions=['sepal_width', 'sepal_length', 'petal_width','petal_length'],
+#     color_continuous_scale=px.colors.diverging.Tealrose,
+#     color_continuous_midpoint=2
+# )
+# fig.show()
+
+demoCsvFile = "DataFiles/Crime and Demographics/filtered_CensusTract_Demographic_Data.csv"
